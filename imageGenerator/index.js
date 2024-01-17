6dc5fcfcd9a6317f94898b1142f9a2b1735c3ecc
journeywithcode <journeywithcode2@gmail.com>
2024-01-17 11:51:43 +0530

    ai image generarion

--- imageGenerator/index.js
+++ imageGenerator/index.js
@@ -1,14 +1,14 @@
-const OpenAI = require("openai");
-const apiKey = require('./Apikey');
+import OpenAI from "openai";
+import apiKey from "./Apikey.js";
 
 const openai = new OpenAI({
   apiKey: apiKey,
 });
 
 async function main() {
-    const image = await openai.images.generate({ model: "dall-e-3", prompt: "" });
+    const image = await openai.images.generate({ model: "dall-e-3", prompt: "journeywithcode" });
+
     console.log(image.data);
 }
 
 main();
-
